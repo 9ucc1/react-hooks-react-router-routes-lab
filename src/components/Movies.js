cd6431fe8cb85b6a04442cb61dc09ff9bef7b11a
Gucci Fan <gucc@umich.edu>
2022-10-26 18:47:15 -0400

    commit tree df4cf6f853042e43970171c88e2d347dc2070657

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  return <>
+    <h1>Movies Page</h1>
+    {movies.map(movie =>{
+      return (
+        <div>
+          <ul>Name: {movie.title}</ul>
+          <ul>Time: {movie.time}</ul>
+          <ul>Genres: 
+            {movie.genres.map(genre=>{
+              return (<li>{genre}</li>)
+            })}
+          </ul>
+        </div>
+        )
+    })}
+  </>;
 }
 
 export default Movies;
